36532f559f2b54cb67248d0d86d0c4cf166d3350
Sandesh Grangdan <sandesh.tamang@innovatetech.co>
2024-06-12 23:15:57 +0545

    Fixes for scanning.

--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import express from "express";
 import multer from "multer";
 import path from "path";
 import NodeClam from "clamscan";
+import fs from "fs";
+
+interface resultTypes {
+  isInfected: boolean;
+  viruses: string[];
+  file? : string;
+}
 
 const app = express();
 
@@ -16,36 +24,79 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage });
+const limits = {
+  fileSize: 300 * 1024 * 1024 // 300 MB limit
+};
+
+const upload = multer({ storage,limits });
+
+const deleteLocalFile = (localDiskFilePath: string) => fs.unlink(localDiskFilePath, (err) => {
+  const message = err ? `Error deleting file: ${err.message}` : 'File deleted successfully';
+  console.log(message);
+});
 
 (async () => {
   try {
     const clamScan = await new NodeClam().init({
       preference: "clamdscan",
-      scanRecursively: true,
+      scanRecursively: false,
       clamdscan: {
-        host: "127.0.0.1",
-        port: 3310,
+      //  multiscan: true,
+        socket: "/var/run/clamav/clamd.ctl",
+      //  host: "127.0.0.1",
+      //  port: 3310,
       },
     });
 
+   app.get("/", async (_req, res) => res.json({message: "Nameste"}));
+
     app.post("/upload", upload.single("file"), async (req, res) => {
       try {
-        const { file } = req;
-        if (!file) {
-          return res.status(400);
+        const { file , body : { store }  } = req;
+        let { source, path, region, error } = JSON.parse(store || '{}');
+        
+        if (!file) return res.status(400).json({message:"File not found"});
+
+        const { filename, path: localDiskFilePath} = file;
+        
+        const result : resultTypes = await clamScan.scanFile(localDiskFilePath);
+
+        if (!result.isInfected && (source || path)) {
+          try {
+            const s3Client = new S3Client({
+              region : region || "ap-southeast-1"
+            });
+            await s3Client.send(new PutObjectCommand({
+              Bucket: source,
+              Key: path,
+              Body: fs.readFileSync(localDiskFilePath),
+              ContentType: file.mimetype
+            }));
+
+            console.log(`File ${filename} uploaded successfully s3://${source}${path}.`);
+          } catch (err) {
+            error = err
+            console.log(err);
+          }
         }
-        const result = await clamScan.scanFile(file.path);
-        console.log(result);
-        console.log(result);
-        return res.json(true);
+
+        deleteLocalFile(localDiskFilePath);
+        delete result?.file;
+        return res.json({
+          result,
+          source,
+          path,
+          error
+        });
+
       } catch (err) {
         console.log(err);
       }
     });
 
-    app.listen(5000, "0.0.0.0", () => {
-      console.log("Listening on port 5000");
+    const port = 8080;
+    app.listen(port, "0.0.0.0", () => {
+      console.log(`Listening on port ${port}`);
     });
   } catch (err) {
     console.log(err);
